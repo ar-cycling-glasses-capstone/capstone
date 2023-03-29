074da817c1280f0a25b1c1e72b273e36035d4c7a
Carlos M <murillc@mcmaster.ca>
2023-03-29 16:35:16 -0400

    Init display class

--- display.py
+++ display.py
@@ -1,93 +1,122 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import Adafruit_GPIO as GPIO
-import Adafruit_GPIO.SPI as SPI
-import spidev
-
-import ST7789 as TFT
-import datetime
-from time import sleep
-
-from PIL import Image, ImageDraw, ImageFont, ImageColor
-
-import numpy as np
-
-import detect
-import cv2
-
+import RPi.GPIO as GPIO
+import pygame
+import time
 import util
 
-# Raspberry Pi pin configuration:
-RST = 25
-DC  = 24
-LED = 19
-SPI_PORT = 0
-SPI_DEVICE = 0
-SPI_MODE = 0b11
-SPI_SPEED_HZ = 40000000
+class Display:
 
-# _spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE)
-# _spi=SPI.SpiDev(0, 0, max_speed_hz=SPI_SPEED_HZ)
-_spi = spidev.SpiDev(SPI_PORT, SPI_DEVICE)
-_spi.max_speed_hz=SPI_SPEED_HZ
+    ASSETS_PATH = "./assets/"
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
 
-disp = TFT.ST7789(spi=_spi, mode=SPI_MODE, rst=RST, dc=DC, led=LED)
+    def __init__(self):
+        pygame.init()
 
-# Initialize display.
-disp.begin()
+        # CLASS VARIABLES
+        self.hud_toggles = {
+            "blindspot": True,
+            "biometrics": True,
+            "weather": True,
+            "bike_stats": True,
+        }
 
-# Clear display.
-disp.clear()
+        
 
-# Analogue clock setting
-width = 240
-height = 240
+        self.done = False
+        self.width  = pygame.display.Info().current_w
+        self.height = pygame.display.Info().current_h
 
-# Initial screen
-image2 = Image.open('Lum_For_Ants.jpg')
-image2.thumbnail((240, 240), Image.ANTIALIAS)
-image2 = util.expand2square(image2, (0,0,0))
+        # LAUNCH IMAGE
+        self.image0 = pygame.image.load(Display.ASSETS_PATH + "Lum_For_Ants.jpg")
+        self.image0 = pygame.transform.scale(image0, (self.width, self.height))
 
-disp.display(image2)
 
-# font setting
-font = ImageFont.load_default()
-fontJ = ImageFont.truetype('DejaVuSans.ttf', 16, encoding='unic')
 
-sleep(1)
+    def main(self):
+        self.init_display();
 
-image1 = Image.new("RGB", (disp.width, disp.height), "WHITE")
-draw = ImageDraw.Draw(image1)
-util.draw_rotated_text(image1, "Blindspot", (30,10), 0, font=fontJ, fill=(0,0,0) )
-util.draw_rotated_text(image1, "Indicator", (30,20), 0, font=fontJ, fill=(0,0,0) )
+        time.sleep(5)
 
-disp.display(image1)
+        self.main_loop();
 
-CarVideo = cv2.VideoCapture('cars.mp4')
-counter = 0
-while CarVideo.isOpened():
-    ret, frame = CarVideo.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    controlkey = cv2.waitKey(1)
-    if ret:        
-        cars_frame, in_blindspot = detect.detect_cars_and_pedestrain(gray)
-        cv2.imshow('frame', cars_frame)
-    else:
-        break
-    if controlkey == ord('q'):
-        break
+        sys.exit();
 
-    print("frame %d, true %d" % (counter, in_blindspot))
-    counter += 1
+    def init_display(self):
+        self.screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN)
+        self.screen.blit(image0,(0,0))
+        pygame.display.update()
 
-    if (in_blindspot):
-        draw.ellipse((5,10,25,30), outline=(255,0,0), fill=(255,0,0))
-    else:
-        draw.ellipse((5,10,25,30), outline=(255,255,255), fill=(255,255,255))
+    def main_loop(self):
+        while(not self.done):
+            for event in pygame.event.get():
+                # Kill program if QUIT event or Escape key pressed
+                if event.type == pygame.QUIT:
+                    self.done = True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.done = True
+            
+            self.check_updates_from_app(); # Interface with Felix
+            self.check_updates_from_camera(); # Interface with Sabrina
+            self.check_updates_from_antreceiver(); # Interface with Alex
+            self.update_display();
 
-    disp.display(image1)
+    def check_updates_from_app(self):
+        print("check from app...")
+        time.sleep(1)
 
-CarVideo.release()
-cv2.destroyAllWindows()
+    def check_updates_from_camera(self):
+        print("check from camera...")
+        time.sleep(1)
 
+    def check_updates_from_antreceiver(self):
+        print("check from ant+...")
+        time.sleep(1)
+
+    def update_display(self):
+        print("updating display...")
+        
+
+        # TEST FONTS
+        font0 = pygame.font.Font('freesansbold.ttf', 32)
+        text0 = font.render('Test', True, Display.BLACK)
+        font1 = pygame.font.Font('freesansbold.ttf', 64)
+        text1 = font.render('Test', True, Display.BLACK)
+        font2 = pygame.font.Font('freesansbold.ttf', 96)
+        text2 = font.render('Test', True, Display.BLACK)
+
+        self.display.fill(Display.WHITE)
+        self.display.blit(text0, (10, 100));
+        self.display.blit(text1, (10, 200));
+        self.display.blit(text2, (10, 300));
+
+        # UPDATE BLINDSPOT
+        if (self.hud_toggles["blindspot"]):
+            pass
+        else:
+            pass
+        
+        # UPDATE WEATHER
+        if (self.hud_toggles["weather"]):
+            pass
+        else:
+            pass
+        
+        # UPDATE BIKE STATS
+        if (self.hud_toggles["bike_stats"]):
+            pass
+        else:
+            pass
+        
+        # UPDATE BIOMETRICS
+        if (self.hud_toggles["biometrics"]):
+            pass
+        else:
+            pass
+
+
+        pygame.display.update()
+
+if __name__ == "__main__":
+    display = Display()
+    display.main()
